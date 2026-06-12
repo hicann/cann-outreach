5ea292e00ad599833d35a192d0cc4d1a803a8cfa
h_13162371206 <cyrishec007@gmail.com>
2026-06-12 10:45:30 +0800

    feat: 淬火深圳，ascend_c算子开发
    Co-authored-by: 何程<cyrishec007@gmail.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !378 merge master into master
    
    feat: 淬火深圳，ascend_c算子开发
    
    Created-by: h_13162371206
    Commit-by: 何程
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    
    <!-- 本 PR 做了什么，为什么需要 / What does this PR do and why -->
    
    ## 改动类型 / Change Type
    
    - [ ] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [ ] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [ ] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [ ] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/cann-outreach!378

---
+++ 2026/cuihuo/shenzhen/hecheng/TanhCustom/op_kernel/tanh_custom.cpp
@@ -0,0 +1,89 @@
+#include "kernel_operator.h"
+#include "tanh_custom_tiling.h"
+
+using namespace AscendC;
+constexpr int32_t BUFFER_NUM = 2; // tensor num for each queue
+
+class KernelTanh {
+public:
+    __aicore__ inline KernelTanh() {}
+    __aicore__ inline void Init(GM_ADDR x, GM_ADDR y, uint32_t totalLength, uint32_t tileNum)
+    {
+        ASSERT(GetBlockNum() != 0 && "block dim can not be zero!");
+        this->blockLength = totalLength / GetBlockNum();
+        this->tileNum = tileNum;
+        ASSERT(tileNum != 0 && "tile num can not be zero!");
+        this->tileLength = this->blockLength / tileNum / BUFFER_NUM;
+
+        xGm.SetGlobalBuffer((__gm__ DTYPE_X *)x + this->blockLength * GetBlockIdx(), this->blockLength);
+        yGm.SetGlobalBuffer((__gm__ DTYPE_Y *)y + this->blockLength * GetBlockIdx(), this->blockLength);
+
+        pipe.InitBuffer(inQueueX, BUFFER_NUM, this->tileLength * sizeof(DTYPE_X));
+        pipe.InitBuffer(outQueueY, BUFFER_NUM, this->tileLength * sizeof(DTYPE_Y));
+        pipe.InitBuffer(tmpBuf0, this->tileLength * sizeof(DTYPE_X));
+        pipe.InitBuffer(tmpBuf1, this->tileLength * sizeof(DTYPE_X));
+        pipe.InitBuffer(tmpBuf2, this->tileLength * sizeof(DTYPE_X));
+    }
+    __aicore__ inline void Process()
+    {
+        int32_t loopCount = this->tileNum * BUFFER_NUM;
+        for (int32_t i = 0; i < loopCount; i++) {
+            CopyIn(i);
+            Compute(i);
+            CopyOut(i);
+        }
+    }
+
+private:
+    __aicore__ inline void CopyIn(int32_t progress)
+    {
+        LocalTensor<DTYPE_X> xLocal = inQueueX.AllocTensor<DTYPE_X>();
+        DataCopy(xLocal, xGm[progress * this->tileLength], this->tileLength);
+        inQueueX.EnQue(xLocal);
+    }
+    __aicore__ inline void Compute(int32_t progress)
+    {
+        LocalTensor<DTYPE_X> xLocal = inQueueX.DeQue<DTYPE_X>();
+        LocalTensor<DTYPE_Y> yLocal = outQueueY.AllocTensor<DTYPE_Y>();
+        LocalTensor<DTYPE_X> expX = tmpBuf0.Get<DTYPE_X>();
+        LocalTensor<DTYPE_X> expNegX = tmpBuf1.Get<DTYPE_X>();
+        LocalTensor<DTYPE_X> denominator = tmpBuf2.Get<DTYPE_X>();
+
+        Exp(expX, xLocal, this->tileLength);
+        Muls(expNegX, xLocal, (DTYPE_X)-1.0, this->tileLength);
+        Exp(expNegX, expNegX, this->tileLength);
+        Sub(yLocal, expX, expNegX, this->tileLength);
+        Add(denominator, expX, expNegX, this->tileLength);
+        Div(yLocal, yLocal, denominator, this->tileLength);
+
+        outQueueY.EnQue<DTYPE_Y>(yLocal);
+        inQueueX.FreeTensor(xLocal);
+    }
+    __aicore__ inline void CopyOut(int32_t progress)
+    {
+        LocalTensor<DTYPE_Y> yLocal = outQueueY.DeQue<DTYPE_Y>();
+        DataCopy(yGm[progress * this->tileLength], yLocal, this->tileLength);
+        outQueueY.FreeTensor(yLocal);
+    }
+
+private:
+    AscendC::TPipe pipe;
+    AscendC::TQue<AscendC::QuePosition::VECIN, BUFFER_NUM> inQueueX;
+    AscendC::TQue<AscendC::QuePosition::VECOUT, BUFFER_NUM> outQueueY;
+    AscendC::TBuf<AscendC::TPosition::VECCALC> tmpBuf0,tmpBuf1,tmpBuf2;
+    AscendC::GlobalTensor<DTYPE_X> xGm;
+    AscendC::GlobalTensor<DTYPE_Y> yGm;
+    uint32_t blockLength;
+    uint32_t tileNum;
+    uint32_t tileLength;
+};
+
+extern "C" __global__ __aicore__ void tanh_custom(GM_ADDR x, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling) {
+    REGISTER_TILING_DEFAULT(TanhCustomTilingData);
+    GET_TILING_DATA(tilingData, tiling);
+    KernelTanh op;
+    op.Init(x, y, tilingData.totalLength, tilingData.tileNum);
+    if (TILING_KEY_IS(1)) {
+        op.Process();
+    }
+}
