da205c938acf15f94e57c215c641cb74fc5f2ddb
u011021720 <zhaoyao92@qq.com>
2026-06-17 11:36:06 +0800

    feat: 新增zhaoyao的南京昇腾淬火-TanhCustom算子工程
    Co-authored-by: geiao223<zhaoyao92@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !537 merge master into master
    
    feat: 新增zhaoyao的南京昇腾淬火-TanhCustom算子工程
    
    Created-by: u011021720
    Commit-by: geiao223
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    
    <!-- 本 PR 做了什么，为什么需要 / What does this PR do and why -->
    
    ## 改动类型 / Change Type
    
    - [ ] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [ ] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [ ] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [ ] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/cann-outreach!537

---
+++ 2026/cuihuo/nanjing/zhaoyao/TanhCustom/op_host/tanh_custom.cpp
@@ -0,0 +1,78 @@
+
+#include "../op_kernel/tanh_custom_tiling.h"
+#include "register/op_def_registry.h"
+
+
+namespace optiling {
+const uint32_t BLOCK_DIM = 8;
+const uint32_t TILE_NUM = 8;
+static ge::graphStatus TilingFunc(gert::TilingContext* context)
+{
+
+    // TODO: 考生自行补齐Tiling函数
+    uint32_t totalLength = context->GetInputShape(0)->GetOriginShape().GetShapeSize();
+    
+    context->SetBlockDim(8);
+
+    size_t *currentWorkspace = context->GetWorkspaceSizes(1);
+    if (currentWorkspace != nullptr) {
+        currentWorkspace[0] = 0;
+    }
+
+    TanhCustomTilingData tiling;
+    tiling.totalLength = totalLength;
+    tiling.tileNum = 8;
+
+    auto rawTilingData = context->GetRawTilingData();
+    rawTilingData->SetDataSize(sizeof(TanhCustomTilingData));
+    memcpy(rawTilingData->GetData(), &tiling, sizeof(TanhCustomTilingData));
+
+    return ge::GRAPH_SUCCESS;
+}
+}
+
+
+namespace ge {
+static ge::graphStatus InferShape(gert::InferShapeContext* context)
+{
+    const gert::Shape* x1_shape = context->GetInputShape(0);
+    gert::Shape* y_shape = context->GetOutputShape(0);
+    *y_shape = *x1_shape;
+    return GRAPH_SUCCESS;
+}
+static ge::graphStatus InferDataType(gert::InferDataTypeContext *context)
+{
+    const auto inputDataType = context->GetInputDataType(0);
+    context->SetOutputDataType(0, inputDataType);
+    return ge::GRAPH_SUCCESS;
+}
+}
+
+
+namespace ops {
+class TanhCustom : public OpDef {
+public:
+    explicit TanhCustom(const char* name) : OpDef(name)
+    {
+        this->Input("x")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16})
+            .Format({ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND});
+        this->Output("y")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16})
+            .Format({ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND});
+
+        this->SetInferShape(ge::InferShape).SetInferDataType(ge::InferDataType);
+
+        this->AICore()
+            .SetTiling(optiling::TilingFunc);
+        this->AICore().AddConfig("ascend910b");
+
+    }
+};
+
+OP_ADD(TanhCustom);
+}
