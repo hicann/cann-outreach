3cb27d36d5075440012d9b7f94eb5bf279bd03e8
2401_87018147 <ivyvivi666@gmail.com>
2026-07-08 15:19:30 +0800

    add lession1
    Co-authored-by: 2401_87018147<ivyvivi666@gmail.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !706 merge master into master
    
    add lession1
    
    Created-by: 2401_87018147
    Commit-by: 2401_87018147
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    
    <!-- 本 PR 做了什么，为什么需要 / What does this PR do and why -->
    
    ## 改动类型 / Change Type
    
    - [ ] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [ ] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [ ] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [ ] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/cann-outreach!706

---
+++ 2026/CANN-Summer-Camps/Xian-Jiaotong-University/2401_87018147/Lession1/build/CMakeFiles/mul_test.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/workspace/gitCode/2401_87018147/cann-outreach/2026/CANN-Summer-Camps/Xian-Jiaotong-University/2401_87018147/Lession1/mul_custom.asc" "CMakeFiles/mul_test.dir/mul_custom.asc.o" "gcc" "CMakeFiles/mul_test.dir/mul_custom.asc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
