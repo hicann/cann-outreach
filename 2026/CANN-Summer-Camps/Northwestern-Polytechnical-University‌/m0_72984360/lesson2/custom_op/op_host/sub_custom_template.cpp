20f8c8ff7a708a1818fdf89bb14209b6369f5ced
m0_72984360 <1805315972@qq.com>
2026-07-10 20:15:34 +0800

    [CANN SUMMER CAMPS 2026][NPU]add lesson2
    Co-authored-by: m0_72984360<1805315972@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !713 merge lesson2 into master
    
    [CANN SUMMER CAMPS 2026][NPU]add lesson2
    
    Created-by: m0_72984360
    Commit-by: m0_72984360
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    
    <!-- 本 PR 做了什么，为什么需要 / What does this PR do and why -->
    
    ## 改动类型 / Change Type
    
    - [ ] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [ ] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [ ] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [ ] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/cann-outreach!713

---
+++ 2026/CANN-Summer-Camps/Northwestern-Polytechnical-University‌/m0_72984360/lesson2/custom_op/op_host/sub_custom_template.cpp
@@ -0,0 +1,71 @@
+/**
+* Copyright (c) 2025 Huawei Technologies Co., Ltd.
+* This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+* CANN Open Software License Agreement Version 2.0 (the "License").
+* Please refer to the License for details. You may not use this file except in compliance with the License.
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+* See LICENSE in the root of the software repository for the full text of the License.
+*/
+
+
+#include "register/op_def_registry.h"
+#include "../op_kernel/sub_custom_template_tiling.h"
+#include "graph/utils/type_utils.h"
+#include "tiling/platform/platform_ascendc.h"
+
+namespace optiling {
+static ge::graphStatus TilingFunc(gert::TilingContext *context)
+{
+    TilingDataTemplate *tiling = context->GetTilingData<TilingDataTemplate>();
+    uint32_t totalLength = context->GetInputShape(0)->GetOriginShape().GetShapeSize();
+    context->SetBlockDim(8);
+    tiling->totalLength = totalLength;
+    tiling->tileNum = 1;
+    
+    return ge::GRAPH_SUCCESS;
+}
+}  // namespace optiling
+
+namespace ge {
+static graphStatus InferShape(gert::InferShapeContext *context)
+{
+    const gert::Shape *intputShape = context->GetInputShape(0);
+    gert::Shape *outputShape = context->GetOutputShape(0);
+    *outputShape = *intputShape;
+    return GRAPH_SUCCESS;
+}
+
+static graphStatus InferDataType(gert::InferDataTypeContext *context)
+{
+    context->SetOutputDataType(0, context->GetInputDataType(0));
+    return ge::GRAPH_SUCCESS;
+}
+}  // namespace ge
+
+namespace ops {
+class SubCustomTemplate : public OpDef {
+public:
+    explicit SubCustomTemplate(const char *name) : OpDef(name)
+    {
+        this->Input("x")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Input("y")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Output("z")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+
+        this->SetInferShape(ge::InferShape).SetInferDataType(ge::InferDataType);
+        this->AICore()
+            .SetTiling(optiling::TilingFunc)
+            .AddConfig("ascend910b");
+    }
+};
+OP_ADD(SubCustomTemplate);
+}  // namespace ops
---
+++ 2026/CANN-Summer-Camps/Northwestern-Polytechnical-University‌/m0_72984360/lesson2/custom_op/op_kernel/sub_custom_template_tiling.h
@@ -0,0 +1,20 @@
+/**
+* Copyright (c) 2025 Huawei Technologies Co., Ltd.
+* This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+* CANN Open Software License Agreement Version 2.0 (the "License").
+* Please refer to the License for details. You may not use this file except in compliance with the License.
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+* See LICENSE in the root of the software repository for the full text of the License.
+*/
+
+
+#ifndef SUB_CUSTOM_TEMPLATE_TILING_H
+#define SUB_CUSTOM_TEMPLATE_TILING_H
+#include <cstdint>
+
+struct TilingDataTemplate {
+    uint32_t totalLength;
+    uint32_t tileNum;
+};
+#endif // SUB_CUSTOM_TEMPLATE_TILING_H
