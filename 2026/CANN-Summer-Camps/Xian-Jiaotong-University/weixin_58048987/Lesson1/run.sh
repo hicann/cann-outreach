7ab5978d3c740a05f71d849f1d4f7b01549bbec1
cann-robot <cann@cann.team>
2026-07-29 16:13:09 +0800

    [CANN SUMMER CAMPS 2026][XJTU] feat: complete Lesson1 MulCustom
    Co-authored-by: Alastor<1253618274@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1370 merge lesson1-mul-custom-weixin-58048987 into master
    
    [CANN SUMMER CAMPS 2026][XJTU] feat: complete Lesson1 MulCustom
    
    Created-by: weixin_58048987
    Commit-by: Alastor
    Merged-by: cann-robot
    Description: ## 变更描述 / Description
    完成西安交通大学 CANN Summer Camps 2026 Lesson1 作业：
    - 新增 weixin_58048987/Lesson1
    - 使用 Ascend C 实现 float32、Shape (8, 2048) 的矢量乘法算子
    - 采用 8 核并行和双缓冲流水
    
    ## 测试信息 / Testing
    - [x] 静态分块检查通过：完整覆盖 16384 个元素
    - [ ] 当前本机缺少 CANN/Ascend 设备，未执行 bash run.sh
    
    ## 检查清单 / Checklist
    - [x] 代码符合目录规范
    - [x] 无硬编码敏感信息
    - [x] 仅提交 Lesson1 相关文件
    
    See merge request: cann/cann-outreach!1370

---
+++ 2026/CANN-Summer-Camps/Xian-Jiaotong-University/weixin_58048987/Lesson1/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+source "${ASCEND_TOOLKIT_HOME}/set_env.sh"
+mkdir -p build
+export ASC_DIR="${ASCEND_HOME_PATH}/aarch64-linux/tikcpp/ascendc_kernel_cmake/"
+cd build
+cmake ..
+make
+./mul_test
