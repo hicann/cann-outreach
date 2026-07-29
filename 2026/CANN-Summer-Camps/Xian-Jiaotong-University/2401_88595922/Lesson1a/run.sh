db8c578089c8eebd5c131e9e10d68d065a4df811
2401_88595922 <zahpamy@qq.com>
2026-07-29 17:39:26 +0800

    [CANN SUMMER CAMPS 2026][XJTU]add Lession1a
    Co-authored-by: 2401_88595922<zahpamy@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1606 merge master into master
    
    [CANN SUMMER CAMPS 2026][XJTU]add Lession1a
    
    Created-by: 2401_88595922
    Commit-by: 2401_88595922
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    
    <!-- 本 PR 做了什么，为什么需要 / What does this PR do and why -->
    	基于下面的代码补充实现mul_custom.asc，使用Ascend C编程实现矢量乘法算子mul_custom。
    	数据类型：输入与输出均为 float 类型。
    	数据形状（Shape）： 输入Shape为 (8, 2048)，输出Shape与输入相同
    	数据布局（Format）：ND
        
    ## 改动类型 / Change Type
    - [ ] Bug 修复 / Bug Fix
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [ ] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [ ] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [ ] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/cann-outreach!1606

---
+++ 2026/CANN-Summer-Camps/Xian-Jiaotong-University/2401_88595922/Lesson1a/run.sh
@@ -0,0 +1,8 @@
+set -euo pipefail
+source $ASCEND_TOOLKIT_HOME/set_env.sh
+mkdir -p build
+export ASC_DIR="$ASCEND_HOME_PATH/aarch64-linux/tikcpp/ascendc_kernel_cmake/" && \
+cd build/ && \
+cmake .. && \
+make
+./mul_test
