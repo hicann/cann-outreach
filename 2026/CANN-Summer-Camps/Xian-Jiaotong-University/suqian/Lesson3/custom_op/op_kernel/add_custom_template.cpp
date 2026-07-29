cf74c08d91e7b0b42809bb6895f2096a2a3cd4c9
2401_87963461 <3113305553@qq.com>
2026-07-29 17:39:44 +0800

    [CANN SUMMER CAMPS 2026][XJTU][Lesson3]
    Co-authored-by: suqian<3113305553@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1541 merge lesson3 into master
    
    [CANN SUMMER CAMPS 2026][XJTU][Lesson3]
    
    Created-by: 2401_87963461
    Commit-by: suqian
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    
    朱浩文第三次作业
    
    ## 改动类型 / Change Type
    
    - [ ] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [ ] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [ ] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [ ] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/cann-outreach!1541

---
+++ 2026/CANN-Summer-Camps/Xian-Jiaotong-University/suqian/Lesson3/custom_op/op_kernel/add_custom_template.cpp
@@ -0,0 +1,106 @@
+#include "kernel_operator.h"
+#include "add_custom_template_tiling.h"
+
+constexpr int32_t BUFFER_NUM = 2;
+
+template <class dtypeX, class dtypeY, class dtypeZ>
+class KernelAdd {
+public:
+    __aicore__ inline KernelAdd() {}
+    __aicore__ inline void Init(GM_ADDR x, GM_ADDR y, GM_ADDR z, uint32_t totalLength, uint32_t tileNum)
+    {
+        uint32_t blockIdx = AscendC::GetBlockIdx();
+        uint32_t blockNum = AscendC::GetBlockNum();
+        
+        this->blockLength = totalLength / blockNum;
+        this->tileNum = tileNum;
+        this->tileLength = this->blockLength / tileNum;
+        
+
+        xGm.SetGlobalBuffer((__gm__ dtypeX *)x + blockIdx * this->blockLength, this->blockLength);
+        yGm.SetGlobalBuffer((__gm__ dtypeY *)y + blockIdx * this->blockLength, this->blockLength);
+        zGm.SetGlobalBuffer((__gm__ dtypeZ *)z + blockIdx * this->blockLength, this->blockLength);
+        
+        pipe.InitBuffer(inQueueX, BUFFER_NUM, this->tileLength * sizeof(dtypeX));
+        pipe.InitBuffer(inQueueY, BUFFER_NUM, this->tileLength * sizeof(dtypeY));
+        pipe.InitBuffer(outQueueZ, BUFFER_NUM, this->tileLength * sizeof(dtypeZ));
+    }
+
+    __aicore__ inline void Process()
+    {
+        CopyIn(0);
+        CopyIn(1);
+
+        Compute(0);
+
+        for (uint32_t i = 2; i < this->tileNum; i++) {
+            CopyIn(i);
+            Compute(i-1);
+            CopyOut(i-2);
+        }
+
+        Compute(this->tileNum - 1);  
+        CopyOut(this->tileNum - 2); 
+        CopyOut(this->tileNum - 1);  
+    }
+
+private:
+    __aicore__ inline void CopyIn(uint32_t tileIdx)
+    {
+
+        AscendC::LocalTensor<dtypeX> xLocal = inQueueX.AllocTensor<dtypeX>();
+        AscendC::LocalTensor<dtypeY> yLocal = inQueueY.AllocTensor<dtypeY>();
+        
+        AscendC::DataCopy(xLocal, xGm[tileIdx * this->tileLength], this->tileLength);
+        AscendC::DataCopy(yLocal, yGm[tileIdx * this->tileLength], this->tileLength);
+        
+        inQueueX.EnQue(xLocal);
+        inQueueY.EnQue(yLocal);
+    }
+
+    __aicore__ inline void Compute(uint32_t tileIdx)
+    {
+
+        AscendC::LocalTensor<dtypeX> xLocal = inQueueX.DeQue<dtypeX>();
+        AscendC::LocalTensor<dtypeY> yLocal = inQueueY.DeQue<dtypeY>();
+        AscendC::LocalTensor<dtypeZ> zLocal = outQueueZ.AllocTensor<dtypeZ>();
+        
+        AscendC::Add(zLocal, xLocal, yLocal, this->tileLength);
+        
+        outQueueZ.EnQue<dtypeZ>(zLocal);
+        inQueueX.FreeTensor(xLocal);
+        inQueueY.FreeTensor(yLocal);
+    }
+
+    __aicore__ inline void CopyOut(uint32_t tileIdx)
+    {
+
+        AscendC::LocalTensor<dtypeZ> zLocal = outQueueZ.DeQue<dtypeZ>();
+        AscendC::DataCopy(zGm[tileIdx * this->tileLength], zLocal, this->tileLength);
+        outQueueZ.FreeTensor(zLocal);
+    }
+
+private:
+    AscendC::TPipe pipe;
+    AscendC::TQue<AscendC::TPosition::VECIN, BUFFER_NUM> inQueueX;
+    AscendC::TQue<AscendC::TPosition::VECIN, BUFFER_NUM> inQueueY;
+    AscendC::TQue<AscendC::TPosition::VECOUT, BUFFER_NUM> outQueueZ;
+    
+    AscendC::GlobalTensor<dtypeX> xGm;
+    AscendC::GlobalTensor<dtypeY> yGm;
+    AscendC::GlobalTensor<dtypeZ> zGm;
+    
+    uint32_t blockLength;
+    uint32_t tileNum;
+    uint32_t tileLength;
+};
+
+__global__ __aicore__ void add_custom_template(GM_ADDR x, GM_ADDR y, GM_ADDR z, GM_ADDR workspace, GM_ADDR tiling)
+{
+    REGISTER_TILING_DEFAULT(AddCustomTemplateTilingData);
+    GET_TILING_DATA_WITH_STRUCT(AddCustomTemplateTilingData, tiling_data, tiling);
+    
+    KernelAdd<DTYPE_X, DTYPE_Y, DTYPE_Z> op;
+    op.Init(x, y, z, tiling_data.totalLength, tiling_data.tileNum);
+    op.Process();
+}
