a9def88b38ec5571f5dd54c86beee696ec51e38e
haozi4263 <329672369@qq.com>
2026-09-01 10:22:33 +0800

    [2026武汉燃星行动&代码侦探·燃星武汉专场]-新增TanhCustom算子工程
    Co-authored-by: haozi4263<329672369@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2336 merge master into master
    
    [2026武汉燃星行动&代码侦探·燃星武汉专场]-新增TanhCustom算子工程
    
    Created-by: haozi4263
    Commit-by: haozi4263
    Merged-by: cann-robot
    Description: [2026武汉燃星行动&代码侦探·燃星武汉专场]-新增TanhCustom算子工程
    
    See merge request: cann/cann-outreach!2336

---
+++ 2026/ranxing/wuhan/haozi4263/TanhCustom/build_out/op_host/CMakeFiles/cust_op_proto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cust_op_proto.dir/__/autogen/op_proto.cc.o"
+  "CMakeFiles/cust_op_proto.dir/__/autogen/op_proto.cc.o.d"
+  "CMakeFiles/cust_op_proto.dir/tanh_custom.cpp.o"
+  "CMakeFiles/cust_op_proto.dir/tanh_custom.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cust_op_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
