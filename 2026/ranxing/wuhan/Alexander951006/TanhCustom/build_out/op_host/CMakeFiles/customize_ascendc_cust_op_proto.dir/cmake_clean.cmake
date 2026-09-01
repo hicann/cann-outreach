8a5460b8f40f22656cebaba9dd7dfb41ad65fa50
Alexander951006 <1053870367@qq.com>
2026-09-01 10:23:06 +0800

    [2026武汉燃星行动&代码侦探·燃星武汉专场]-新增TanhCustom算子工程
    Co-authored-by: Alexander951006<1053870367@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2348 merge master into master
    
    [2026武汉燃星行动&代码侦探·燃星武汉专场]-新增TanhCustom算子工程
    
    Created-by: Alexander951006
    Commit-by: Alexander951006
    Merged-by: cann-robot
    Description: 
    <!-- 感谢您的合入申请! -->
    
    ### 当前PR是否有AI参与:   
    [ ]  否     
    
    [ x]  是   
    _x_1.  AI Agent 平台:    
    __2.  AI 模型:  
    __3.  Prompt上下文 : 
    
    
    ### PR功能描述 / 为什么需要这个合入**:  
    <!-- 本 PR 做了什么，为什么需要 / What does this PR do and why -->     
     tanh算子实现      
        
    ### 该PR关联的issue   
    *(格式为fixes #<issue号>, 或者resolves #<issue号>)*: fixes #
    
    ### 希望检视人员了解:
    
    
    ## 改动类型 / Change Type
    
    - [ ] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [ ] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [ ] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [ ] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/cann-outreach!2348

---
+++ 2026/ranxing/wuhan/Alexander951006/TanhCustom/build_out/op_host/CMakeFiles/customize_ascendc_cust_op_proto.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "libcust_opsproto_rt2.0.pdb"
+  "libcust_opsproto_rt2.0.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/customize_ascendc_cust_op_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
