7cc53574008d91d99b7d2051e84991a7f196b411
cann-robot <cann@cann.team>
2026-09-05 18:53:00 +0800

    [CANN训练营-东南大学专场] 东南大学 - op_02_mul
    Co-authored-by: 卓歆力<xinli_zhuo@sina.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2927 merge submit/seu_gcw_E72aNbbF_02/op_02_mul into master
    
    [CANN训练营-东南大学专场] 东南大学 - op_02_mul
    
    Created-by: gcw_E72aNbbF
    Commit-by: 卓歆力
    Merged-by: cann-robot
    Description: # 东南大学
    
    ## 团队信息
    
    - 提交者: 卓歆力 (xinli_zhuo@sina.com)
    - 身份: 学生
    - 单位: 东南大学
    
    ## 成员
    
    - 卓歆力 (gcw_E72aNbbF): 提交者, xinli_zhuo@sina.com
    
    ## 算子: op_02_mul
    
    实现矢量：z=x*y
    
    
    See merge request: cann/cann-outreach!2927

---
+++ 2026/CANN-Campus-Camps/Southeast-University/seu_gcw_E72aNbbF_02/code/op_02_mul/op_host/mul.cpp
@@ -0,0 +1,77 @@
+// Host侧Tiling实现
+#include "register/op_def_registry.h"
+#include "tiling/platform/platform_ascendc.h"
+
+#include "../op_kernel/mul_tiling.h"
+#include "../op_kernel/tiling_key_mul.h"
+
+namespace optiling {
+    static ge::graphStatus TilingFunc(gert::TilingContext *context) {
+        // 示例: 获取平台信息
+        auto platform = platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
+        int32_t num_cores_aiv = platform.GetCoreNumAiv();
+        uint64_t ub_size;
+        platform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, ub_size);
+        // 示例: 获取算子输入数组信息
+        const gert::Tensor *tensor_x = context->GetRequiredInputTensor(0);
+        const gert::Tensor *tensor_y = context->GetRequiredInputTensor(1);
+        ge::DataType dtype_x = tensor_x->GetDataType(); // 获取数据类型
+        int dtype_size_x = ge::GetSizeByDataType(dtype_x); // 获取数据类型的字长
+        uint32_t length_x = tensor_x->GetShapeSize(); // 获取元素个数
+        uint32_t size_x = tensor_x->GetSize(); // 获取内存大小
+        // 示例: 配置tiling key, 从而实现kernel侧不同数据类型/算法的区分
+        uint32_t DT_X = static_cast<uint32_t>(dtype_x);
+        ASCENDC_TPL_SEL_PARAM(context, DT_X);
+        // 示例: 计算tiling方案并填充tiling结构体
+        MulTilingData *tiling = context->GetTilingData<MulTilingData>();
+        tiling->length = length_x;
+        
+        tiling->tileNum = 2;
+
+        // 配置启动核数
+        context->SetBlockDim(8);
+        // 配置workspace大小
+        size_t *currentWorkspace = context->GetWorkspaceSizes(1);
+        currentWorkspace[0] = 0;
+        return ge::GRAPH_SUCCESS;
+    }
+}  // namespace optiling
+
+namespace ge {
+    static graphStatus InferShape(gert::InferShapeContext *context) {
+        const gert::Shape* x1_shape = context->GetInputShape(0);
+        gert::Shape* z_shape = context->GetOutputShape(0);
+        *z_shape = *x1_shape;
+        return GRAPH_SUCCESS;
+    }
+    static graphStatus InferDataType(gert::InferDataTypeContext *context) {
+        const auto inputDataType = context->GetInputDataType(0);
+        context->SetOutputDataType(0, inputDataType);
+        return ge::GRAPH_SUCCESS;
+    }
+}  // namespace ge
+
+namespace ops {
+    class Mul : public OpDef {
+    public:
+        explicit Mul(const char *name) : OpDef(name) {
+            this->Input("x")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+            this->Input("y")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+            this->Output("z")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+            this->SetInferShape(ge::InferShape).SetInferDataType(ge::InferDataType);
+            this->AICore()
+                .SetTiling(optiling::TilingFunc)
+                .AddConfig("ascend910b");
+        }
+    };
+    OP_ADD(Mul);
+}  // namespace ops
