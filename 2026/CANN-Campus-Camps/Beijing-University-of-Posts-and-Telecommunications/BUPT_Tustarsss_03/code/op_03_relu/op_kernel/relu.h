6622612f638f9016ffa08c780d8d4b6c9fb628d8
cann-robot <cann@cann.team>
2026-09-06 08:58:19 +0800

    [CANN训练营-北京邮电大学专场] 北京邮电大学 - op_03_relu
    Co-authored-by: Tustarsss<18222498435@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3306 merge submit/BUPT_Tustarsss_03/op_03_relu into master
    
    [CANN训练营-北京邮电大学专场] 北京邮电大学 - op_03_relu
    
    Created-by: Tustarsss
    Commit-by: Tustarsss
    Merged-by: cann-robot
    Description: # 北京邮电大学
    
    ## 团队信息
    
    - 提交者: Tustarsss (18222498435@163.com)
    - 身份: 学生
    - 单位: 北京邮电大学
    
    ## 成员
    
    - Tustarsss (Tustarsss): 提交者, 18222498435@163.com
    
    ## 算子: op_03_relu
    
    
    
    
    See merge request: cann/cann-outreach!3306

---
+++ 2026/CANN-Campus-Camps/Beijing-University-of-Posts-and-Telecommunications/BUPT_Tustarsss_03/code/op_03_relu/op_kernel/relu.h
@@ -0,0 +1,101 @@
+/*!
+ * \file relu.h
+ * \brief Relu 算子 kernel 类定义
+ */
+
+#ifndef RELU_H
+#define RELU_H
+
+#include "kernel_operator.h"
+#include "kernel_tiling/kernel_tiling.h"
+#include "relu_tiling_data.h"
+#include "relu_tiling_key.h"
+
+namespace NsRelu {
+
+using namespace AscendC;
+
+constexpr int32_t BUFFER_NUM = 2;
+
+template <typename T>
+class Relu {
+public:
+    __aicore__ inline Relu(){};
+
+    __aicore__ inline void Init(GM_ADDR x, GM_ADDR y, const ReluTilingData* tilingData);
+    __aicore__ inline void Process();
+
+private:
+    __aicore__ inline void CopyIn(int64_t progress, int64_t currentNum);
+    __aicore__ inline void CopyOut(int64_t progress, int64_t currentNum);
+    __aicore__ inline void Compute(int64_t currentNum);
+
+private:
+    TPipe pipe;
+    TQue<QuePosition::VECIN, BUFFER_NUM> inputQueueX;
+    TQue<QuePosition::VECOUT, BUFFER_NUM> outputQueueY;
+
+    GlobalTensor<T> inputGMX;
+    GlobalTensor<T> outputGMY;
+
+    int64_t blockLength_ = 0;
+    int64_t ubLength_ = 0;
+};
+
+template <typename T>
+__aicore__ inline void Relu<T>::Init(GM_ADDR x, GM_ADDR y, const ReluTilingData* tilingData)
+{
+    const int64_t blockOffset = tilingData->blockFactor * GetBlockIdx();
+    const int64_t remaining = tilingData->totalNum - blockOffset;
+    blockLength_ = remaining < tilingData->blockFactor ? remaining : tilingData->blockFactor;
+    ubLength_ = tilingData->ubFactor;
+    inputGMX.SetGlobalBuffer((__gm__ T*)x + blockOffset, blockLength_);
+    outputGMY.SetGlobalBuffer((__gm__ T*)y + blockOffset, blockLength_);
+    pipe.InitBuffer(inputQueueX, BUFFER_NUM, ubLength_ * sizeof(T));
+    pipe.InitBuffer(outputQueueY, BUFFER_NUM, ubLength_ * sizeof(T));
+}
+
+template <typename T>
+__aicore__ inline void Relu<T>::CopyIn(int64_t progress, int64_t currentNum)
+{
+    LocalTensor<T> xLocal = inputQueueX.AllocTensor<T>();
+    DataCopy(xLocal, inputGMX[progress * ubLength_], currentNum);
+    inputQueueX.EnQue(xLocal);
+}
+
+template <typename T>
+__aicore__ inline void Relu<T>::Compute(int64_t currentNum)
+{
+    LocalTensor<T> xLocal = inputQueueX.DeQue<T>();
+    LocalTensor<T> yLocal = outputQueueY.AllocTensor<T>();
+    AscendC::Relu(yLocal, xLocal, currentNum);
+    outputQueueY.EnQue(yLocal);
+    inputQueueX.FreeTensor(xLocal);
+}
+
+template <typename T>
+__aicore__ inline void Relu<T>::CopyOut(int64_t progress, int64_t currentNum)
+{
+    LocalTensor<T> yLocal = outputQueueY.DeQue<T>();
+    DataCopy(outputGMY[progress * ubLength_], yLocal, currentNum);
+    outputQueueY.FreeTensor(yLocal);
+}
+
+template <typename T>
+__aicore__ inline void Relu<T>::Process()
+{
+    if (blockLength_ <= 0 || ubLength_ <= 0) {
+        return;
+    }
+    const int64_t loopCount = (blockLength_ + ubLength_ - 1) / ubLength_;
+    for (int64_t i = 0; i < loopCount; ++i) {
+        const int64_t remaining = blockLength_ - i * ubLength_;
+        const int64_t currentNum = remaining < ubLength_ ? remaining : ubLength_;
+        CopyIn(i, currentNum);
+        Compute(currentNum);
+        CopyOut(i, currentNum);
+    }
+}
+
+} // namespace NsRelu
+#endif // RELU_H
