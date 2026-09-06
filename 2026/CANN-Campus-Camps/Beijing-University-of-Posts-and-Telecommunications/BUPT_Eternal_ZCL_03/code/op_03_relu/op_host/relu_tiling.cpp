0f4175b95a7e6c88845a50fdc8acda092eb2c870
cann-robot <cann@cann.team>
2026-09-06 10:32:13 +0800

    [CANN训练营-北京邮电大学专场] 北京邮电大学 - op_03_relu
    Co-authored-by: 赵卓然<2996078603@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2994 merge submit/BUPT_eternal-zcl-03/op_03_relu into master
    
    [CANN训练营-北京邮电大学专场] 北京邮电大学 - op_03_relu
    
    Created-by: Eternal_ZCL
    Commit-by: 赵卓然
    Merged-by: cann-robot
    Description: # 北京邮电大学
    
    ## 团队信息
    
    - 提交者: 赵卓然 (2996078603@qq.com)
    - 身份: 学生
    - 单位: 北京邮电大学
    
    ## 成员
    
    - 赵卓然 (Eternal_ZCL): 提交者, 2996078603@qq.com
    
    ## 算子: op_03_relu
    
    基于 Ascend C 实现支持 float32/float16 的 ReLU 激活算子，采用八核切分、Tiling 与双缓冲流水完成高效计算。
    
    
    See merge request: cann/cann-outreach!2994

---
+++ 2026/CANN-Campus-Camps/Beijing-University-of-Posts-and-Telecommunications/BUPT_Eternal_ZCL_03/code/op_03_relu/op_host/relu_tiling.cpp
@@ -0,0 +1,84 @@
+/*!
+ * \file relu_tiling.cpp
+ * \brief Relu 算子 Tiling 实现
+ */
+
+#include "register/op_def_registry.h"
+#include "op_common/log/log.h"
+#include "op_common/op_host/util/math_util.h"
+#include "op_common/op_host/util/platform_util.h"
+#include "../op_kernel/relu_tiling_data.h"
+#include "../op_kernel/relu_tiling_key.h"
+
+namespace optiling {
+
+constexpr uint32_t WS_SYS_SIZE = 0U;
+constexpr int64_t TILE_NUM = 8;
+
+static ge::graphStatus GetPlatformInfo(gert::TilingContext* context,
+                                       uint64_t& ubSize, int64_t& coreNum)
+{
+    fe::PlatFormInfos* platformInfoPtr = context->GetPlatformInfo();
+    OP_CHECK_NULL_WITH_CONTEXT(context, platformInfoPtr);
+    auto platform = platform_ascendc::PlatformAscendC(platformInfoPtr);
+    coreNum = platform.GetCoreNumAiv();
+    OP_CHECK_IF(coreNum == 0, OP_LOGE(context, "coreNum is 0"), return ge::GRAPH_FAILED);
+    platform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, ubSize);
+    OP_CHECK_IF(ubSize == 0, OP_LOGE(context, "ubSize is 0"), return ge::GRAPH_FAILED);
+    return ge::GRAPH_SUCCESS;
+}
+
+static ge::graphStatus GetWorkspaceSize(gert::TilingContext* context)
+{
+    size_t* workspace = context->GetWorkspaceSizes(1);
+    OP_CHECK_NULL_WITH_CONTEXT(context, workspace);
+    workspace[0] = WS_SYS_SIZE;
+    return ge::GRAPH_SUCCESS;
+}
+
+static ge::graphStatus ReluTilingFunc(gert::TilingContext* context)
+{
+    uint64_t ubSize = 0;
+    int64_t availableCoreNum = 0;
+    OP_CHECK_IF(GetPlatformInfo(context, ubSize, availableCoreNum) != ge::GRAPH_SUCCESS,
+                OP_LOGE(context, "GetPlatformInfo error"), return ge::GRAPH_FAILED);
+    OP_CHECK_IF(GetWorkspaceSize(context) != ge::GRAPH_SUCCESS,
+                OP_LOGE(context, "GetWorkspaceSize error"), return ge::GRAPH_FAILED);
+
+    ReluTilingData* tiling = context->GetTilingData<ReluTilingData>();
+    OP_CHECK_NULL_WITH_CONTEXT(context, tiling);
+    const gert::StorageShape* xShape = context->GetInputShape(0);
+    OP_CHECK_NULL_WITH_CONTEXT(context, xShape);
+
+    int64_t totalNum = 1;
+    for (int i = 0; i < xShape->GetStorageShape().GetDimNum(); ++i) {
+        totalNum *= xShape->GetStorageShape().GetDim(i);
+    }
+
+    const int64_t blockDim = availableCoreNum < 8 ? availableCoreNum : 8;
+    tiling->totalNum = totalNum;
+    tiling->blockFactor = totalNum / blockDim;
+    tiling->ubFactor = tiling->blockFactor / TILE_NUM;
+    context->SetBlockDim(blockDim);
+
+    uint64_t tilingKey;
+    auto inputDesc = context->GetInputDesc(0);
+    if (inputDesc != nullptr &&
+        (inputDesc->GetDataType() == ge::DT_FLOAT16 || inputDesc->GetDataType() == ge::DT_BF16)) {
+        tilingKey = GET_TPL_TILING_KEY(RELU_TPL_SCH_MODE_0);
+    } else {
+        tilingKey = GET_TPL_TILING_KEY(RELU_TPL_SCH_MODE_1);
+    }
+    context->SetTilingKey(tilingKey);
+    return ge::GRAPH_SUCCESS;
+}
+
+static ge::graphStatus TilingParseForRelu([[maybe_unused]] gert::TilingParseContext* context)
+{
+    return ge::GRAPH_SUCCESS;
+}
+
+struct ReluCompileInfo {};
+IMPL_OP_OPTILING(Relu).Tiling(ReluTilingFunc).TilingParse<ReluCompileInfo>(TilingParseForRelu);
+
+} // namespace optiling
