b4705ec89c38e432a10f4f0858266d8fbbfa309b
cann-robot <cann@cann.team>
2026-09-06 10:32:08 +0800

    [CANN训练营-北京邮电大学专场] 北京邮电大学 - op_02_mul
    Co-authored-by: 牛浩凯<2501958879@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3341 merge submit/bupt_starflow2/op_02_mul into master
    
    [CANN训练营-北京邮电大学专场] 北京邮电大学 - op_02_mul
    
    Created-by: 2403_87496454
    Commit-by: 牛浩凯
    Merged-by: cann-robot
    Description: # 北京邮电大学
    
    ## 团队信息
    
    - 提交者: 牛浩凯 (2501958879@qq.com)
    - 身份: 学生
    - 单位: 北京邮电大学
    
    ## 成员
    
    - 牛浩凯 (2403_87496454): 提交者, 2501958879@qq.com
    
    ## 算子: op_02_mul
    
    mul
    
    
    See merge request: cann/cann-outreach!3341

---
+++ 2026/CANN-Campus-Camps/Beijing-University-of-Posts-and-Telecommunications/bupt_starflow2/code/op_02_mul/op_host/mul.cpp
@@ -0,0 +1,72 @@
+// Host渚iling瀹炵幇
+#include "register/op_def_registry.h"
+#include "tiling/platform/platform_ascendc.h"
+
+#include "../op_kernel/mul_tiling.h"
+#include "../op_kernel/tiling_key_mul.h"
+
+namespace optiling {
+    const uint32_t TILE_NUM = 8;
+    static ge::graphStatus TilingFunc(gert::TilingContext *context) {
+        // 绀轰緥: 鑾峰彇绠楀瓙杈撳叆鏁扮粍淇℃伅
+        const gert::Tensor *tensor_x = context->GetRequiredInputTensor(0);
+        const gert::Tensor *tensor_y = context->GetRequiredInputTensor(1);
+        uint32_t DT_X = static_cast<uint32_t>(tensor_x->GetDataType());
+        ASCENDC_TPL_SEL_PARAM(context, DT_X);
+        MulTilingData *tiling = context->GetTilingData<MulTilingData>();
+        const gert::StorageShape* x1_shape = context->GetInputShape(0);
+        int32_t data_sz = 1;
+        for (int i = 0; i < x1_shape->GetStorageShape().GetDimNum(); i++)
+            data_sz *= x1_shape->GetStorageShape().GetDim(i);
+        tiling->totalLength = data_sz;
+        tiling->tileNum = TILE_NUM;
+        // 閰嶇疆鍚姩鏍告暟
+        context->SetBlockDim(8);
+        // 閰嶇疆workspace澶у皬
+        size_t *currentWorkspace = context->GetWorkspaceSizes(1);
+        currentWorkspace[0] = 0;
+        return ge::GRAPH_SUCCESS;
+    }
+}  // namespace optiling
+
+namespace ge {
+    static graphStatus InferShape(gert::InferShapeContext *context) {
+        // 鑾峰彇绗?鍙疯緭鍏?x 鐨?shape锛堥潤鎬?鍔ㄦ€佺淮搴︿俊鎭級
+        const gert::Shape* x1_shape = context->GetInputShape(0);
+        // 鑾峰彇绗?鍙疯緭鍑?z 鐨?shape 鍙啓鎸囬拡锛岀敤浜庤缃緭鍑?shape
+        gert::Shape* z_shape = context->GetOutputShape(0);
+        // 閫愬厓绱犱箻娉曪細杈撳嚭 shape 涓庤緭鍏?x 瀹屽叏涓€鑷?
+        *z_shape = *x1_shape;
+        return GRAPH_SUCCESS;
+    }
+    static graphStatus InferDataType(gert::InferDataTypeContext *context) {
+        const auto inputDataType = context->GetInputDataType(0);
+        context->SetOutputDataType(0, inputDataType);
+        return ge::GRAPH_SUCCESS;
+    }
+}  // namespace ge
+
+namespace ops {
+    class Mul : public OpDef {
+    public:
+        explicit Mul(const char *name) : OpDef(name) {
+            this->Input("x")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+            this->Input("y")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+            this->Output("z")
+                .ParamType(REQUIRED)
+                .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+                .Format({ge::FORMAT_ND, ge::FORMAT_ND});
+            this->SetInferShape(ge::InferShape).SetInferDataType(ge::InferDataType);
+            this->AICore()
+                .SetTiling(optiling::TilingFunc)
+                .AddConfig("ascend910b");
+        }
+    };
+    OP_ADD(Mul);
+}  // namespace ops
---
+++ 2026/CANN-Campus-Camps/Beijing-University-of-Posts-and-Telecommunications/bupt_starflow2/code/op_02_mul/op_kernel/mul_tiling.h
@@ -0,0 +1,9 @@
+// Tiling缁撴瀯浣撳畾涔夌殑澶存枃浠?
+#pragma once
+
+#include <cstdint>
+
+struct MulTilingData {
+    uint32_t totalLength;
+    uint32_t tileNum;
+};
