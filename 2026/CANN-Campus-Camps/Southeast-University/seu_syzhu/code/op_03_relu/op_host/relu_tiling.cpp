d3e2f281e8eed93ff66857850cb8601317379771
sy_zhu <sy.zhu@seu.edu.cn>
2026-09-08 02:33:35 +0800

    [CANN训练营-东南大学专场] 东南大学 - op_03_relu
    Co-authored-by: sy_zhu<sy.zhu@seu.edu.cn>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2822 merge submit/seu_syzhu/op_03_relu into master
    
    [CANN训练营-东南大学专场] 东南大学 - op_03_relu
    
    Created-by: sy_zhu
    Commit-by: sy_zhu
    Merged-by: cann-robot
    Description: # 东南大学
    
    ## 团队信息
    
    - 提交者: sy_zhu (sy.zhu@seu.edu.cn)
    - 身份: 学生
    - 单位: 东南大学
    
    ## 成员
    
    - sy_zhu (sy_zhu): 提交者, sy.zhu@seu.edu.cn
    
    ## 算子: op_03_relu
    
    
    
    
    See merge request: cann/cann-outreach!2822

---
+++ 2026/CANN-Campus-Camps/Southeast-University/seu_syzhu/code/op_03_relu/op_host/relu_tiling.cpp
@@ -0,0 +1,282 @@
+/*!
+ * \file relu_tiling.cpp
+ * \brief Relu 算子 Tiling 实现
+ */
+
+#include "register/op_def_registry.h"
+#include "op_common/log/log.h"
+#include "op_common/op_host/util/math_util.h"
+#include "op_common/op_host/util/platform_util.h"
+#include "../op_kernel/relu_tiling_data.h"
+#include "../op_kernel/relu_tiling_key.h"
+
+namespace optiling {
+
+using Ops::Base::CeilDiv;
+using Ops::Base::CeilAlign;
+using Ops::Base::FloorDiv;
+using Ops::Base::FloorAlign;
+using Ops::Base::GetUbBlockSize;
+
+constexpr uint32_t WS_SYS_SIZE = 0U;
+
+static ge::graphStatus GetPlatformInfo(
+    gert::TilingContext* context,
+    uint64_t& ubSize,
+    int64_t& coreNum)
+{
+    fe::PlatFormInfos* platformInfoPtr =
+        context->GetPlatformInfo();
+
+    OP_CHECK_NULL_WITH_CONTEXT(
+        context,
+        platformInfoPtr);
+
+    auto ascendcPlatform =
+        platform_ascendc::PlatformAscendC(
+            platformInfoPtr);
+
+    coreNum =
+        ascendcPlatform.GetCoreNumAiv();
+
+    OP_CHECK_IF(
+        coreNum == 0,
+        OP_LOGE(
+            context,
+            "coreNum is 0"),
+        return ge::GRAPH_FAILED);
+
+    ascendcPlatform.GetCoreMemSize(
+        platform_ascendc::CoreMemType::UB,
+        ubSize);
+
+    OP_CHECK_IF(
+        ubSize == 0,
+        OP_LOGE(
+            context,
+            "ubSize is 0"),
+        return ge::GRAPH_FAILED);
+
+    return ge::GRAPH_SUCCESS;
+}
+
+
+static ge::graphStatus GetWorkspaceSize(
+    gert::TilingContext* context)
+{
+    size_t* currentWorkspace =
+        context->GetWorkspaceSizes(1);
+
+    OP_CHECK_NULL_WITH_CONTEXT(
+        context,
+        currentWorkspace);
+
+    currentWorkspace[0] =
+        WS_SYS_SIZE;
+
+    return ge::GRAPH_SUCCESS;
+}
+
+
+static ge::graphStatus ReluTilingFunc(
+    gert::TilingContext* context)
+{
+    uint64_t ubSize = 0;
+    int64_t coreNum = 0;
+
+    OP_CHECK_IF(
+        GetPlatformInfo(
+            context,
+            ubSize,
+            coreNum)
+            != ge::GRAPH_SUCCESS,
+        OP_LOGE(
+            context,
+            "GetPlatformInfo error"),
+        return ge::GRAPH_FAILED);
+
+    OP_CHECK_IF(
+        GetWorkspaceSize(
+            context)
+            != ge::GRAPH_SUCCESS,
+        OP_LOGE(
+            context,
+            "GetWorkspaceSize error"),
+        return ge::GRAPH_FAILED);
+
+    const gert::StorageShape* inputShape =
+        context->GetInputShape(0);
+
+    OP_CHECK_NULL_WITH_CONTEXT(
+        context,
+        inputShape);
+
+    int64_t totalNum =
+        inputShape
+            ->GetStorageShape()
+            .GetShapeSize();
+
+    OP_CHECK_IF(
+        totalNum <= 0,
+        OP_LOGE(
+            context,
+            "totalNum is invalid"),
+        return ge::GRAPH_FAILED);
+
+    auto inputDesc =
+        context->GetInputDesc(0);
+
+    OP_CHECK_NULL_WITH_CONTEXT(
+        context,
+        inputDesc);
+
+    ge::DataType dtype =
+        inputDesc->GetDataType();
+
+    int64_t typeSize =
+        ge::GetSizeByDataType(
+            dtype);
+
+    OP_CHECK_IF(
+        typeSize <= 0,
+        OP_LOGE(
+            context,
+            "invalid dtype"),
+        return ge::GRAPH_FAILED);
+
+    constexpr int64_t ROW_LENGTH =
+        2048;
+
+    int64_t rowNum =
+        totalNum /
+        ROW_LENGTH;
+
+    OP_CHECK_IF(
+        rowNum <= 0,
+        OP_LOGE(
+            context,
+            "rowNum is invalid"),
+        return ge::GRAPH_FAILED);
+
+    int64_t usedCoreNum =
+        coreNum;
+
+    if (usedCoreNum > rowNum) {
+        usedCoreNum =
+            rowNum;
+    }
+
+    while (
+        usedCoreNum > 1 &&
+        rowNum % usedCoreNum != 0)
+    {
+        --usedCoreNum;
+    }
+
+    if (usedCoreNum <= 0) {
+        usedCoreNum = 1;
+    }
+
+    int64_t rowsPerCore =
+        rowNum /
+        usedCoreNum;
+
+    int64_t blockFactor =
+        rowsPerCore *
+        ROW_LENGTH;
+
+    constexpr int64_t BUFFER_NUM_HOST =
+        2;
+
+    constexpr int64_t TENSOR_NUM =
+        2;
+
+    int64_t maxUbElements =
+        static_cast<int64_t>(
+            ubSize)
+        /
+        (
+            BUFFER_NUM_HOST *
+            TENSOR_NUM *
+            typeSize
+        );
+
+    int64_t rowsPerUb =
+        maxUbElements /
+        ROW_LENGTH;
+
+    if (rowsPerUb < 1) {
+        rowsPerUb = 1;
+    }
+
+    if (rowsPerUb > rowsPerCore) {
+        rowsPerUb =
+            rowsPerCore;
+    }
+
+    int64_t ubFactor =
+        rowsPerUb *
+        ROW_LENGTH;
+
+    ReluTilingData* tiling =
+        context
+            ->GetTilingData<
+                ReluTilingData>();
+
+    OP_CHECK_NULL_WITH_CONTEXT(
+        context,
+        tiling);
+
+    tiling->totalNum =
+        totalNum;
+
+    tiling->blockFactor =
+        blockFactor;
+
+    tiling->ubFactor =
+        ubFactor;
+
+    context->SetBlockDim(
+        static_cast<uint32_t>(
+            usedCoreNum));
+
+    uint64_t tilingKey;
+
+    if (dtype == ge::DT_FLOAT16) {
+        tilingKey =
+            GET_TPL_TILING_KEY(
+                RELU_TPL_SCH_MODE_0);
+    } else {
+        tilingKey =
+            GET_TPL_TILING_KEY(
+                RELU_TPL_SCH_MODE_1);
+    }
+
+    context->SetTilingKey(
+        tilingKey);
+
+    return ge::GRAPH_SUCCESS;
+}
+
+
+static ge::graphStatus
+TilingParseForRelu(
+    [[maybe_unused]]
+    gert::TilingParseContext* context)
+{
+    return ge::GRAPH_SUCCESS;
+}
+
+
+struct ReluCompileInfo {};
+
+
+IMPL_OP_OPTILING(Relu)
+    .Tiling(
+        ReluTilingFunc)
+    .TilingParse<
+        ReluCompileInfo>(
+            TilingParseForRelu);
+
+
+} // namespace optiling
