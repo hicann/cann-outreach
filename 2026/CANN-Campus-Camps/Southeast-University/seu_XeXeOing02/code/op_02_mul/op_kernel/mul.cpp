51df0f648967368bb229522c11507c6f3aaa325e
cann-robot <cann@cann.team>
2026-09-07 22:21:10 +0800

    [CANN训练营-东南大学专场] 东南大学 - op_02_mul
    Co-authored-by: XeXeOing<1991305535@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2999 merge submit/seu_XeXeOing02/op_02_mul into master
    
    [CANN训练营-东南大学专场] 东南大学 - op_02_mul
    
    Created-by: XeXeOing
    Commit-by: XeXeOing
    Merged-by: cann-robot
    Description: # 东南大学
    
    ## 团队信息
    
    - 提交者: XeXeOing (1991305535@qq.com)
    - 身份: 学生
    - 单位: 东南大学
    
    ## 成员
    
    - XeXeOing (XeXeOing): 提交者, 1991305535@qq.com
    
    ## 算子: op_02_mul
    
    
    
    
    See merge request: cann/cann-outreach!2999

---
+++ 2026/CANN-Campus-Camps/Southeast-University/seu_XeXeOing02/code/op_02_mul/op_kernel/mul.cpp
@@ -0,0 +1,100 @@
+// Kernel侧核函数实现
+#include "kernel_operator.h"
+#include "mul_tiling.h"
+#include "tiling_key_mul.h"
+
+constexpr int32_t BUFFER_NUM = 2;
+constexpr int32_t BLOCK_NUM = 8;
+
+template <class DT_X>
+class KernelMul {
+public:
+    __aicore__ inline KernelMul() {}
+    __aicore__ inline void Init(GM_ADDR x, GM_ADDR y, GM_ADDR z, uint32_t totalLength) {
+        uint32_t blockIdx = AscendC::GetBlockIdx();
+        uint32_t baseLen = totalLength / BLOCK_NUM;
+        uint32_t remainLen = totalLength % BLOCK_NUM;
+
+        // 余数交给最后一个block处理
+        if(blockIdx == BLOCK_NUM - 1) {
+            this->blockLength = baseLen + remainLen;
+        } else {
+            this->blockLength = baseLen;
+        }
+
+        // 只按双缓冲切分，不再额外8次细分，放大单次计算长度
+        this->tileLength = this->blockLength / BUFFER_NUM;
+
+        uint32_t gmOffset = baseLen * blockIdx;
+        xGm.SetGlobalBuffer((__gm__ DT_X*)x + gmOffset, this->blockLength);
+        yGm.SetGlobalBuffer((__gm__ DT_X*)y + gmOffset, this->blockLength);
+        zGm.SetGlobalBuffer((__gm__ DT_X*)z + gmOffset, this->blockLength);
+
+        pipe.InitBuffer(inQueueX, BUFFER_NUM, this->tileLength * sizeof(DT_X));
+        pipe.InitBuffer(inQueueY, BUFFER_NUM, this->tileLength * sizeof(DT_X));
+        pipe.InitBuffer(outQueueZ, BUFFER_NUM, this->tileLength * sizeof(DT_X));
+    }
+
+    __aicore__ inline void Process() {
+        // 循环次数 = BUFFER_NUM，只有2轮，减少迭代开销，解决超时
+        int32_t loopCount = BUFFER_NUM;
+        for (int32_t i = 0; i < loopCount; i++) {
+            CopyIn(i);
+            Compute(i);
+            CopyOut(i);
+        }
+    }
+
+private:
+    __aicore__ inline void CopyIn(int32_t progress)
+    {
+        AscendC::LocalTensor<DT_X> xLocal = inQueueX.AllocTensor<DT_X>();
+        AscendC::LocalTensor<DT_X> yLocal = inQueueY.AllocTensor<DT_X>();
+        AscendC::DataCopy(xLocal, xGm[progress * this->tileLength], this->tileLength);
+        AscendC::DataCopy(yLocal, yGm[progress * this->tileLength], this->tileLength);
+        inQueueX.EnQue(xLocal);
+        inQueueY.EnQue(yLocal);
+    }
+
+    __aicore__ inline void Compute(int32_t progress)
+    {
+        AscendC::LocalTensor<DT_X> xLocal = inQueueX.DeQue<DT_X>();
+        AscendC::LocalTensor<DT_X> yLocal = inQueueY.DeQue<DT_X>();
+        AscendC::LocalTensor<DT_X> zLocal = outQueueZ.AllocTensor<DT_X>();
+
+        AscendC::Mul(zLocal, xLocal, yLocal, this->tileLength);
+
+        outQueueZ.EnQue<DT_X>(zLocal);
+        inQueueX.FreeTensor(xLocal);
+        inQueueY.FreeTensor(yLocal);
+    }
+
+    __aicore__ inline void CopyOut(int32_t progress)
+    {
+        AscendC::LocalTensor<DT_X> zLocal = outQueueZ.DeQue<DT_X>();
+        AscendC::DataCopy(zGm[progress * this->tileLength], zLocal, this->tileLength);
+        outQueueZ.FreeTensor(zLocal);
+    }
+
+private:
+    AscendC::TPipe pipe;
+    AscendC::TQue<AscendC::QuePosition::VECIN, BUFFER_NUM> inQueueX;
+    AscendC::TQue<AscendC::QuePosition::VECIN, BUFFER_NUM> inQueueY;
+    AscendC::TQue<AscendC::QuePosition::VECOUT, BUFFER_NUM> outQueueZ;
+
+    AscendC::GlobalTensor<DT_X> xGm;
+    AscendC::GlobalTensor<DT_X> yGm;
+    AscendC::GlobalTensor<DT_X> zGm;
+
+    uint32_t blockLength;
+    uint32_t tileLength;
+};
+
+template <typename DT_X>
+__global__ __aicore__ void mul(GM_ADDR x, GM_ADDR y, GM_ADDR z, GM_ADDR workspace, GM_ADDR tiling) {
+    REGISTER_TILING_DEFAULT(MulTilingData);
+    GET_TILING_DATA_WITH_STRUCT(MulTilingData, tiling_data, tiling);
+    KernelMul<DT_X> op;
+    op.Init(x, y, z, tiling_data.length);
+    op.Process();
+}
