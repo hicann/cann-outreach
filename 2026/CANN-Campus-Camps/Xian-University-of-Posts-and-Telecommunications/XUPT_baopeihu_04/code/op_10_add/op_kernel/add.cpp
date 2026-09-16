faeaa0cc7b930194aff5b4b07f7102fee898503f
baopeihu <bphdyx@outlook.com>
2026-09-16 10:02:49 +0800

    [CANN训练营-西安邮电大学专场现场实操] 西安邮电大学 - op_10_add
    Co-authored-by: 包培虎<bphdyx@outlook.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2705 merge submit/XUPT_baopeihu_04/op_10_add into master
    
    [CANN训练营-西安邮电大学专场现场实操] 西安邮电大学 - op_10_add
    
    Created-by: baopeihu
    Commit-by: 包培虎
    Merged-by: cann-robot
    Description: # 西安邮电大学
    
    ## 团队信息
    
    - 提交者: 包培虎 (bphdyx@outlook.com)
    - 身份: 学生
    - 单位: 西安邮电大学
    
    ## 成员
    
    - 包培虎 (baopeihu): 提交者, bphdyx@outlook.com
    
    ## 算子: op_10_add
    
    矢量加法
    
    Closes #90
    
    
    See merge request: cann/cann-outreach!2705

---
+++ 2026/CANN-Campus-Camps/Xian-University-of-Posts-and-Telecommunications/XUPT_baopeihu_04/code/op_10_add/op_kernel/add.cpp
@@ -0,0 +1,80 @@
+// Kernel侧核函数实现
+#include "kernel_operator.h"
+
+#include "add_tiling.h"
+#include "tiling_key_add.h"
+constexpr int32_t BUFFER_NUM = 2; // tensor num for each queue
+template <class DT_X>
+class KernelAdd {
+public:
+    __aicore__ inline KernelAdd() {}
+    __aicore__ inline void Init(GM_ADDR x, GM_ADDR y, GM_ADDR z, uint32_t totalLength, uint32_t tileNum) {
+        this->blockLength = totalLength / AscendC::GetBlockNum();
+        this->tileNum = tileNum;
+        this->tileLength = this->blockLength / tileNum / BUFFER_NUM;
+        xGm.SetGlobalBuffer((__gm__ DT_X*)x + this->blockLength * AscendC::GetBlockIdx(), this->blockLength);
+        yGm.SetGlobalBuffer((__gm__ DT_X*)y + this->blockLength * AscendC::GetBlockIdx(), this->blockLength);
+        zGm.SetGlobalBuffer((__gm__ DT_X*)z + this->blockLength * AscendC::GetBlockIdx(), this->blockLength);
+        pipe.InitBuffer(inQueueX, BUFFER_NUM, this->tileLength * sizeof(DT_X));
+        pipe.InitBuffer(inQueueY, BUFFER_NUM, this->tileLength * sizeof(DT_X));
+        pipe.InitBuffer(outQueueZ, BUFFER_NUM, this->tileLength * sizeof(DT_X));
+    }
+    __aicore__ inline void Process() {
+        int32_t loopCount = this->tileNum * BUFFER_NUM;  //循环次数是分块数+BUFFER_NUM
+        for (int32_t i = 0; i < loopCount; i++) {
+            CopyIn(i);
+            Compute(i);
+            CopyOut(i);
+        }
+    }
+private:
+    __aicore__ inline void CopyIn(int32_t progress)
+    {
+        // TODO: 考生自行补齐初始化函数
+        AscendC::LocalTensor<DT_X> xLocal = inQueueX.AllocTensor<DT_X>();
+        AscendC::LocalTensor<DT_X> yLocal = inQueueY.AllocTensor<DT_X>();
+        AscendC::DataCopy(xLocal, xGm[progress * this->tileLength], this->tileLength);
+        AscendC::DataCopy(yLocal, yGm[progress * this->tileLength], this->tileLength);
+        inQueueX.EnQue(xLocal);
+        inQueueY.EnQue(yLocal);
+    }
+    __aicore__ inline void Compute(int32_t progress)
+    {
+        AscendC::LocalTensor<DT_X> xLocal = inQueueX.DeQue<DT_X>();
+        AscendC::LocalTensor<DT_X> yLocal = inQueueY.DeQue<DT_X>();
+        AscendC::LocalTensor<DT_X> zLocal = outQueueZ.AllocTensor<DT_X>();
+
+        AscendC::Add(zLocal, xLocal, yLocal, this->tileLength);
+        // ---- 步骤9：结果入队，释放输入 ----
+        outQueueZ.EnQue<DT_X>(zLocal);
+        inQueueX.FreeTensor(xLocal);
+        inQueueY.FreeTensor(yLocal);
+    }
+    __aicore__ inline void CopyOut(int32_t progress)
+    {
+        // TODO: 考生自行补齐初始化函数
+        AscendC::LocalTensor<DT_X> zLocal = outQueueZ.DeQue<DT_X>();
+        AscendC::DataCopy(zGm[progress * this->tileLength], zLocal, this->tileLength);
+        outQueueZ.FreeTensor(zLocal);
+    }
+private:
+    AscendC::TPipe pipe;
+    AscendC::TQue<AscendC::QuePosition::VECIN, BUFFER_NUM> inQueueX;
+    AscendC::TQue<AscendC::QuePosition::VECIN, BUFFER_NUM> inQueueY;
+    AscendC::TQue<AscendC::QuePosition::VECOUT, BUFFER_NUM> outQueueZ;
+    AscendC::GlobalTensor<DT_X> xGm;
+    AscendC::GlobalTensor<DT_X> yGm;
+    AscendC::GlobalTensor<DT_X> zGm;
+    uint32_t blockLength; //片内总长度
+    uint32_t tileNum;  //片内分块
+    uint32_t tileLength;  //每块长度
+};
+
+template <typename DT_X>
+ __global__ __aicore__ void add(GM_ADDR x, GM_ADDR y, GM_ADDR z, GM_ADDR workspace, GM_ADDR tiling) {
+    REGISTER_TILING_DEFAULT(AddTilingData);
+    GET_TILING_DATA_WITH_STRUCT(AddTilingData, tiling_data, tiling);
+    KernelAdd<DT_X> op;
+    op.Init(x, y, z, tiling_data.totalLength, tiling_data.tileNum);
+    op.Process();
+}
---
+++ 2026/CANN-Campus-Camps/Xian-University-of-Posts-and-Telecommunications/XUPT_baopeihu_04/code/op_10_add/op_kernel/add_tiling.h
@@ -0,0 +1,9 @@
+// Tiling结构体定义的头文件
+#pragma once
+
+#include <cstdint>
+
+struct AddTilingData {
+    uint32_t totalLength;
+    uint32_t tileNum;
+};
