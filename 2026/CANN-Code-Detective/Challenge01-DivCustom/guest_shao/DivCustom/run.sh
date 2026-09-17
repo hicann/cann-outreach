61da4feee504675c97e27a48da56078d9e51deee
guest_shao <319694583@qq.com>
2026-09-17 15:55:31 +0800

    【代码侦探Challenge01-DivCustom】新增DivCustom代码提交PR样例
    Co-authored-by: guest_shao<319694583@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1052 merge challenge01 into master
    
    【代码侦探Challenge01-DivCustom】新增DivCustom代码提交PR样例
    
    Created-by: guest_shao
    Commit-by: guest_shao
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    新增DivCustom代码样例
    <!-- 本 PR 做了什么，为什么需要 / What does this PR do and why -->
    
    ## 改动类型 / Change Type
    
    - [ ] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [x] 其它 / Other
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [ ] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [ ] 代码符合规范 / Code follows style guide
    - [x] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [x] 提交信息符合规范 / Commit message follows convention
    注意：以下截图正式提交需要修改为验证通过的截图
    ![image.png](https://raw.gitcode.com/user-images/assets/9865789/b2109301-af48-452e-a597-2e390ff524a4/image.png 'image.png')
    
    See merge request: cann/cann-outreach!1052

---
+++ 2026/CANN-Code-Detective/Challenge01-DivCustom/guest_shao/DivCustom/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 激活cann环境，可根据实际情况修改，在线环境一般不用修改，
+source "$ASCEND_TOOLKIT_HOME/set_env.sh"
+mkdir -p build
+export ASC_DIR="$ASCEND_HOME_PATH/aarch64-linux/tikcpp/ascendc_kernel_cmake/"
+cd build/ && \
+cmake .. && \
+make && \
+./div_test
